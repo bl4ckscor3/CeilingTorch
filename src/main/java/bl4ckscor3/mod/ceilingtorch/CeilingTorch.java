7fa0fe23fdecd4d7e843e529e4972c0a75a366b9
bl4ckscor3 <bl4ckscor33@gmail.com>
2020-07-30 22:51:03 +0200

    add way to register compat from a different mod

--- src/main/java/bl4ckscor3/mod/ceilingtorch/CeilingTorch.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/CeilingTorch.java
@@ -1,7 +1,7 @@
 package bl4ckscor3.mod.ceilingtorch;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Supplier;
 
 import bl4ckscor3.mod.ceilingtorch.compat.bonetorch.BoneTorchCompat;
@@ -27,39 +27,39 @@ public class CeilingTorch
 {
 	public static final String MODID = "ceilingtorch";
 	public static final String NAME = "Ceiling Torch";
-	private static List<Supplier<ICeilingTorchCompat>> compatList = new ArrayList<>();
+	private static final Map<String,Supplier<ICeilingTorchCompat>> COMPAT_LIST = new HashMap<>();
 	private static boolean modernityLoaded;
 
 	public CeilingTorch()
 	{
-		compatList.add(VanillaCompat::new);
-
-		if(ModList.get().isLoaded("ilikewood"))
-			compatList.add(ILikeWoodCompat::new);
+		COMPAT_LIST.put("minecraft", VanillaCompat::new);
 
 		if(ModList.get().isLoaded("bonetorch"))
-			compatList.add(BoneTorchCompat::new);
+			COMPAT_LIST.put("bonetorch", BoneTorchCompat::new);
 
 		if(ModList.get().isLoaded("druidcraft"))
-			compatList.add(DruidcraftCompat::new);
+			COMPAT_LIST.put("druidcraft", DruidcraftCompat::new);
 
-		if(ModList.get().isLoaded("torchmaster"))
-			compatList.add(TorchmasterCompat::new);
-
-		if(ModList.get().isLoaded("upgrade_aquatic"))
-			compatList.add(UpgradeAquaticCompat::new);
-
-		if(ModList.get().isLoaded("silentgear"))
-			compatList.add(SilentGearCompat::new);
+		if(ModList.get().isLoaded("ilikewood"))
+			COMPAT_LIST.put("ilikewood", ILikeWoodCompat::new);
 
 		if(modernityLoaded = ModList.get().isLoaded("modernity"))
-			compatList.add(ModernityCompat::new);
+			COMPAT_LIST.put("modernity", ModernityCompat::new);
+
+		if(ModList.get().isLoaded("silentgear"))
+			COMPAT_LIST.put("silentgear", SilentGearCompat::new);
+
+		if(ModList.get().isLoaded("torchmaster"))
+			COMPAT_LIST.put("torchmaster", TorchmasterCompat::new);
+
+		if(ModList.get().isLoaded("upgrade_aquatic"))
+			COMPAT_LIST.put("upgrade_aquatic", UpgradeAquaticCompat::new);
 	}
 
 	@SubscribeEvent
 	public static void registerBlocks(RegistryEvent.Register<Block> event)
 	{
-		for(Supplier<ICeilingTorchCompat> compat : compatList)
+		for(Supplier<ICeilingTorchCompat> compat : COMPAT_LIST.values())
 		{
 			compat.get().registerBlocks(event);
 		}
@@ -68,10 +68,21 @@ public class CeilingTorch
 	@SubscribeEvent
 	public static void onInterModProcess(InterModProcessEvent event)
 	{
-		for(Supplier<ICeilingTorchCompat> compat : compatList)
+		for(Supplier<ICeilingTorchCompat> compat : COMPAT_LIST.values())
 		{
 			compat.get().registerPlaceEntries();
 		}
+	}
+
+	/**
+	 * Adds ceiling torch compatibility for the mod with the given modid, if that mod is loaded
+	 * @param modid The modid of the mod to add compatibility to
+	 * @param compat The compatibility class
+	 */
+	public static void addCompat(String modid, Supplier<ICeilingTorchCompat> compat)
+	{
+		if(ModList.get().isLoaded(modid))
+			COMPAT_LIST.put(modid, compat);
 	}
 
 	public static boolean isModernityLoaded()
