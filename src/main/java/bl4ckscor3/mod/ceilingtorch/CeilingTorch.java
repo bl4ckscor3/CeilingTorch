b020686c48dfe104e35b35a36366a199661e2f2f
bl4ckscor3 <bl4ckscor33@gmail.com>
2020-08-02 10:55:25 +0200

    add magical torches compat
    #12

--- src/main/java/bl4ckscor3/mod/ceilingtorch/CeilingTorch.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/CeilingTorch.java
@@ -7,6 +7,7 @@ import java.util.function.Supplier;
 
 import bl4ckscor3.mod.ceilingtorch.compat.bambooblocks.BambooBlocksCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.bonetorch.BoneTorchCompat;
+import bl4ckscor3.mod.ceilingtorch.compat.magicaltorches.MagicalTorchesCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.silentgear.SilentGearCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.torchmaster.TorchmasterCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.vanilla.VanillaCompat;
@@ -37,6 +38,9 @@ public class CeilingTorch
 
 		if(ModList.get().isLoaded("bonetorch"))
 			preliminaryCompatList.put("bonetorch", BoneTorchCompat::new);
+
+		if(ModList.get().isLoaded("magical_torches"))
+			preliminaryCompatList.put("magical_torches", MagicalTorchesCompat::new);
 
 		if(ModList.get().isLoaded("silentgear"))
 			preliminaryCompatList.put("silentgear", SilentGearCompat::new);
