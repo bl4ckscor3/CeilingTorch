5370f72b6b20218efcc5e05934a89ffbda5784d9
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-02-13 12:57:57 +0100

    port gaia dimension compat

--- src/main/java/bl4ckscor3/mod/ceilingtorch/CeilingTorch.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/CeilingTorch.java
@@ -10,6 +10,7 @@ import bl4ckscor3.mod.ceilingtorch.compat.bonetorch.BoneTorchCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.druidcraft.DruidcraftCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.dwarfcoal.DwarfCoalCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.essentialfeatues.EssentialFeaturesCompat;
+import bl4ckscor3.mod.ceilingtorch.compat.gaiadimension.GaiaDimensionCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.magicaltorches.MagicalTorchesCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.silentgear.SilentGearCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.tofucraft.TofuCraftCompat;
@@ -51,6 +52,9 @@ public class CeilingTorch
 
 		if(ModList.get().isLoaded("essentialfeatures"))
 			preliminaryCompatList.put("essentialfeatures", EssentialFeaturesCompat::new);
+
+		if(ModList.get().isLoaded("gaiadimension"))
+			preliminaryCompatList.put("gaiadimension", GaiaDimensionCompat::new);
 
 		if(ModList.get().isLoaded("magical_torches"))
 			preliminaryCompatList.put("magical_torches", MagicalTorchesCompat::new);
