159b6432a9d89b5f1c5f30da82058a028d98526c
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-07-31 11:28:28 +0200

    remove compat of not-yet-updated mods

--- src/main/java/bl4ckscor3/mod/ceilingtorch/CeilingTorch.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/CeilingTorch.java
@@ -5,33 +5,9 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Supplier;
 
-import bl4ckscor3.mod.ceilingtorch.compat.atum.AtumCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.bambooblocks.BambooBlocksCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.bonetorch.BoneTorchCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.chaosawakens.ChaosAwakensCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.druidcraft.DruidcraftCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.dwarfcoal.DwarfCoalCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.endergetic.EndergeticCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.essentialfeatues.EssentialFeaturesCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.extendedlights.ExtendedLightsCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.gaiadimension.GaiaDimensionCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.iceandfire.IceAndFireCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.ilikewood.ILikeWoodCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.infernalexpansion.InfernalExpansionCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.inspirations.InspirationsCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.lotr.LotrCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.magicaltorches.MagicalTorchesCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.malum.MalumCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.nethercraft.NethercraftCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.pokecubeaio.PokecubeAIOCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.secretrooms.SecretRoomsCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.silentgear.SilentGearCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.tofucraft.TofuCraftCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.torchbandolier.TorchBandolierCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.torcherino.TorcherinoCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.torchmaster.TorchmasterCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.undergarden.UndergardenCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.upgradeaquatic.UpgradeAquaticCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.vanilla.VanillaCompat;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -60,86 +36,14 @@ public class CeilingTorch
 		preliminaryCompatList.put("minecraft", VanillaCompat::new);
 
 		//cannot use addCompat because then the compat class will be classloaded which may crash if the mod is not present
-		if(ModList.get().isLoaded("atum"))
-			preliminaryCompatList.put("atum", AtumCompat::new);
-
-		if(ModList.get().isLoaded("bamboo_blocks"))
-			preliminaryCompatList.put("bamboo_blocks", BambooBlocksCompat::new);
-
 		if(ModList.get().isLoaded("bonetorch"))
 			preliminaryCompatList.put("bonetorch", BoneTorchCompat::new);
-
-		if(ModList.get().isLoaded("chaosawakens"))
-			preliminaryCompatList.put("chaosawakens", ChaosAwakensCompat::new);
-
-		if(ModList.get().isLoaded("druidcraft"))
-			preliminaryCompatList.put("druidcraft", DruidcraftCompat::new);
-
-		if(ModList.get().isLoaded("dwarfcoal"))
-			preliminaryCompatList.put("dwarfcoal", DwarfCoalCompat::new);
-
-		if(ModList.get().isLoaded("endergetic"))
-			preliminaryCompatList.put("endergetic", EndergeticCompat::new);
-
-		if(ModList.get().isLoaded("essentialfeatures"))
-			preliminaryCompatList.put("essentialfeatures", EssentialFeaturesCompat::new);
-
-		if(ModList.get().isLoaded("extlights"))
-			preliminaryCompatList.put("extlights", ExtendedLightsCompat::new);
-
-		if(ModList.get().isLoaded("gaiadimension"))
-			preliminaryCompatList.put("gaiadimension", GaiaDimensionCompat::new);
-
-		if(ModList.get().isLoaded("iceandfire"))
-			preliminaryCompatList.put("iceandfire", IceAndFireCompat::new);
-
-		if(ModList.get().isLoaded("ilikewood"))
-			preliminaryCompatList.put("ilikewood", ILikeWoodCompat::new);
-
-		if(ModList.get().isLoaded("infernalexp"))
-			preliminaryCompatList.put("infernalexp", InfernalExpansionCompat::new);
-
-		if(ModList.get().isLoaded("inspirations"))
-			preliminaryCompatList.put("inspirations", InspirationsCompat::new);
-
-		if(ModList.get().isLoaded("lotr"))
-			preliminaryCompatList.put("lotr", LotrCompat::new);
 
 		if(ModList.get().isLoaded("magical_torches"))
 			preliminaryCompatList.put("magical_torches", MagicalTorchesCompat::new);
 
-		if(ModList.get().isLoaded("malum"))
-			preliminaryCompatList.put("malum", MalumCompat::new);
-
-		if(ModList.get().isLoaded("nethercraft"))
-			preliminaryCompatList.put("nethercraft", NethercraftCompat::new);
-
-		if(ModList.get().isLoaded("pokecube_legends"))
-			preliminaryCompatList.put("pokecube_legends", PokecubeAIOCompat::new);
-
-		if(ModList.get().isLoaded("secretroomsmod"))
-			preliminaryCompatList.put("secretroomsmod", SecretRoomsCompat::new);
-
-		if(ModList.get().isLoaded("silentgear"))
-			preliminaryCompatList.put("silentgear", SilentGearCompat::new);
-
-		if(ModList.get().isLoaded("tofucraft"))
-			preliminaryCompatList.put("tofucraft", TofuCraftCompat::new);
-
-		if(ModList.get().isLoaded("torcherino"))
-			preliminaryCompatList.put("torcherino", TorcherinoCompat::new);
-
 		if(ModList.get().isLoaded("torchbandolier"))
 			preliminaryCompatList.put("torchbandolier", TorchBandolierCompat::new);
-
-		if(ModList.get().isLoaded("torchmaster"))
-			preliminaryCompatList.put("torchmaster", TorchmasterCompat::new);
-
-		if(ModList.get().isLoaded("undergarden"))
-			preliminaryCompatList.put("undergarden", UndergardenCompat::new);
-
-		if(ModList.get().isLoaded("upgrade_aquatic"))
-			preliminaryCompatList.put("upgrade_aquatic", UpgradeAquaticCompat::new);
 	}
 
 	@SubscribeEvent
