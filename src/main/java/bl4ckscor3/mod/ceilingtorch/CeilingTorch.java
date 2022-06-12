6a3d21593d1ad85284f8825b2a4c7396546816b5
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-06-12 18:34:47 +0200

    remove old mod compat

--- src/main/java/bl4ckscor3/mod/ceilingtorch/CeilingTorch.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/CeilingTorch.java
@@ -5,22 +5,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Supplier;
 
-import bl4ckscor3.mod.ceilingtorch.compat.additionallights.AdditionalLightsCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.aquatictorches.AquaticTorchesCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.bonetorch.BoneTorchCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.ilikewood.ILikeWoodCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.integrateddynamics.IntegratedDynamicsCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.magicaltorches.MagicalTorchesCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.malum.MalumCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.moshiz.MoShizCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.nethersdelight.NethersDelightCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.pokecubeaio.PokecubeAIOCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.secretrooms.SecretRoomsCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.silentgear.SilentGearCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.tofucraft.TofuCraftCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.torchbandolier.TorchBandolierCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.torchmaster.TorchmasterCompat;
-import bl4ckscor3.mod.ceilingtorch.compat.undergarden.UndergardenCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.vanilla.VanillaCompat;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -49,53 +33,6 @@ public class CeilingTorch
 		preliminaryCompatList.put("minecraft", VanillaCompat::new);
 
 		//cannot use addCompat because then the compat class will be classloaded which may crash if the mod is not present
-		if(ModList.get().isLoaded("additional_lights"))
-			preliminaryCompatList.put("additional_lights", AdditionalLightsCompat::new);
-
-		if(ModList.get().isLoaded("aquatictorches"))
-			preliminaryCompatList.put("aquatictorches", AquaticTorchesCompat::new);
-
-		if(ModList.get().isLoaded("bonetorch"))
-			preliminaryCompatList.put("bonetorch", BoneTorchCompat::new);
-
-		if(ModList.get().isLoaded("ilikewood"))
-			preliminaryCompatList.put("ilikewood", ILikeWoodCompat::new);
-
-		if(ModList.get().isLoaded("integrateddynamics"))
-			preliminaryCompatList.put("integrateddynamics", IntegratedDynamicsCompat::new);
-
-		if(ModList.get().isLoaded("magical_torches"))
-			preliminaryCompatList.put("magical_torches", MagicalTorchesCompat::new);
-
-		if(ModList.get().isLoaded("malum"))
-			preliminaryCompatList.put("malum", MalumCompat::new);
-
-		if(ModList.get().isLoaded("ms"))
-			preliminaryCompatList.put("ms", MoShizCompat::new);
-
-		if(ModList.get().isLoaded("nethersdelight"))
-			preliminaryCompatList.put("nethersdelight", NethersDelightCompat::new);
-
-		if(ModList.get().isLoaded("pokecube_legends"))
-			preliminaryCompatList.put("pokecube_legends", PokecubeAIOCompat::new);
-
-		if(ModList.get().isLoaded("secretroomsmod"))
-			preliminaryCompatList.put("secretroomsmod", SecretRoomsCompat::new);
-
-		if(ModList.get().isLoaded("silentgear"))
-			preliminaryCompatList.put("silentgear", SilentGearCompat::new);
-
-		if(ModList.get().isLoaded("torchbandolier"))
-			preliminaryCompatList.put("torchbandolier", TorchBandolierCompat::new);
-
-		if(ModList.get().isLoaded("torchmaster"))
-			preliminaryCompatList.put("torchmaster", TorchmasterCompat::new);
-
-		if(ModList.get().isLoaded("tofucraft"))
-			preliminaryCompatList.put("tofucraft", TofuCraftCompat::new);
-
-		if(ModList.get().isLoaded("undergarden"))
-			preliminaryCompatList.put("undergarden", UndergardenCompat::new);
 	}
 
 	@SubscribeEvent
