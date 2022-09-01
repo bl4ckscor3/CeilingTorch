3f0c3441eecef2bdedd604145e05e880e16b1dd9
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-09-01 16:38:35 +0200

    readd integrated dynamics compat

--- src/main/java/bl4ckscor3/mod/ceilingtorch/CeilingTorch.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/CeilingTorch.java
@@ -8,6 +8,7 @@ import java.util.function.Supplier;
 import bl4ckscor3.mod.ceilingtorch.compat.additionallights.AdditionalLightsCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.aquatictorches.AquaticTorchesCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.bonetorch.BoneTorchCompat;
+import bl4ckscor3.mod.ceilingtorch.compat.integrateddynamics.IntegratedDynamicsCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.magicaltorches.MagicalTorchesCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.torchmaster.TorchmasterCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.vanilla.VanillaCompat;
@@ -51,6 +52,9 @@ public class CeilingTorch
 
 		if(ModList.get().isLoaded("bonetorch"))
 			preliminaryCompatList.put("bonetorch", BoneTorchCompat::new);
+
+		if(ModList.get().isLoaded("integrateddynamics"))
+			preliminaryCompatList.put("integrateddynamics", IntegratedDynamicsCompat::new);
 
 		if(ModList.get().isLoaded("magical_torches"))
 			preliminaryCompatList.put("magical_torches", MagicalTorchesCompat::new);
---
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/compat/integrateddynamics/MenrilCeilingTorchBlock.java
@@ -0,0 +1,22 @@
+package bl4ckscor3.mod.ceilingtorch.compat.integrateddynamics;
+
+import java.util.function.Supplier;
+
+import bl4ckscor3.mod.ceilingtorch.compat.vanilla.CeilingTorchBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class MenrilCeilingTorchBlock extends CeilingTorchBlock
+{
+	public MenrilCeilingTorchBlock(Properties properties, ParticleOptions particleData, Supplier<Block> originalBlock)
+	{
+		super(properties, particleData, originalBlock);
+	}
+
+	@Override
+	public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource rand) {}
+}
