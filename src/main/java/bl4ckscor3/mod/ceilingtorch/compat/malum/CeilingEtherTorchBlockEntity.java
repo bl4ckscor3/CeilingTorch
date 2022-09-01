7eae665461d8364d58953a95e5e3ef039a5aefee
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-09-01 12:47:44 +0200

    fix malum compat
    closes #61

--- src/main/java/bl4ckscor3/mod/ceilingtorch/compat/malum/CeilingEtherTorchBlockEntity.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/compat/malum/CeilingEtherTorchBlockEntity.java
@@ -4,13 +4,15 @@ import java.awt.Color;
 
 import com.sammy.malum.common.blockentity.EtherBlockEntity;
 import com.sammy.malum.core.setup.client.ParticleRegistry;
-import com.sammy.ortus.helpers.ColorHelper;
-import com.sammy.ortus.setup.OrtusParticles;
-import com.sammy.ortus.systems.rendering.particle.ParticleBuilders;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import team.lodestar.lodestone.helpers.ColorHelper;
+import team.lodestar.lodestone.setup.LodestoneParticleRegistry;
+import team.lodestar.lodestone.systems.easing.Easing;
+import team.lodestar.lodestone.systems.rendering.particle.ParticleBuilders;
+import team.lodestar.lodestone.systems.rendering.particle.SimpleParticleOptions.SpecialRemovalProtocol;
 
 public class CeilingEtherTorchBlockEntity extends EtherBlockEntity
 {
@@ -34,14 +36,74 @@ public class CeilingEtherTorchBlockEntity extends EtherBlockEntity
 		float scale = 0.17F + level.random.nextFloat() * 0.03F;
 		float velocity = 0.04F + level.random.nextFloat() * 0.02F;
 
-		ParticleBuilders.create(OrtusParticles.SPARKLE_PARTICLE).setScale(scale * 2.0F, 0.0F).setLifetime(lifeTime).setAlpha(0.2F).setColor(firstColor, secondColor).setColorCurveMultiplier(1.5F).spawn(level, x, y, z);
-		ParticleBuilders.create(OrtusParticles.WISP_PARTICLE).setScale(scale, 0.0F).setLifetime(lifeTime).setAlpha(0.9F, 0.5F).setColor(firstColor, secondColor).setColorCurveMultiplier(2.0F).addMotion(0.0D, velocity, 0.0D).setSpin(level.random.nextFloat() * 0.5F).spawn(level, x, y, z);
+		ParticleBuilders.create(LodestoneParticleRegistry.WISP_PARTICLE)
+		.setScale(scale, 0.0F)
+		.setAlpha(0.75F, 0.25F)
+		.setLifetime(lifeTime)
+		.setColor(firstColor, secondColor)
+		.setColorCoefficient(1.4F)
+		.setColorEasing(Easing.BOUNCE_IN_OUT)
+		.setSpinOffset(level.getGameTime() * 0.2F % 6.28F)
+		.setSpin(0.2F, 0.4F)
+		.setSpinEasing(Easing.QUARTIC_IN)
+		.addMotion(0.0D, velocity, 0.0D)
+		.enableNoClip()
+		.spawn(level, x, y, z);
+		ParticleBuilders.create(LodestoneParticleRegistry.TWINKLE_PARTICLE)
+		.setScale(scale * 2.0F, scale * 0.1F)
+		.setLifetime(lifeTime)
+		.setAlpha(0.25F, 0.0F)
+		.setColor(firstColor, secondColor)
+		.setColorEasing(Easing.SINE_IN)
+		.setColorCoefficient(2.25F)
+		.setSpin(0.0F, 2.0F)
+		.setSpinEasing(Easing.QUARTIC_IN)
+		.enableNoClip()
+		.spawn(level, x, y, z);
 
-		if(level.getGameTime() % 2L == 0L && level.random.nextFloat() < 0.25F)
+		if(level.getGameTime() % 2L == 0L)
 		{
-			y += 0.05000000074505806;
-			ParticleBuilders.create(ParticleRegistry.SPIRIT_FLAME_PARTICLE).setScale(0.75F, 0.0F).setColor(firstColor, secondColor).setColorCurveMultiplier(3.0F).randomOffset(0.15000000596046448D, 0.25D).addMotion(0.0D, 0.019999999552965164D, 0.0D).spawn(level, x, y, z);
-			ParticleBuilders.create(ParticleRegistry.SPIRIT_FLAME_PARTICLE).setScale(0.5F, 0.0F).setColor(firstColor, secondColor).setColorCurveMultiplier(3.0F).randomOffset(0.10000000149011612D, 0.25D).addMotion(0.0D, velocity, 0.0D).spawn(level, x, y, z);
+			y += 0.15000000596046448;
+
+			if(level.random.nextFloat() < 0.5F)
+			{
+				ParticleBuilders.create(ParticleRegistry.SPIRIT_FLAME_PARTICLE)
+				.setScale(0.5F, 0.75F, 0.0F)
+				.setColor(firstColor, secondColor)
+				.setColorEasing(Easing.CIRC_IN_OUT)
+				.setColorCoefficient(2.5F)
+				.setAlpha(0.2F, 1.0F, 0.0F)
+				.setAlphaEasing(Easing.SINE_IN, Easing.QUAD_IN)
+				.setAlphaCoefficient(3.5F)
+				.randomOffset(0.15000000596046448D, 0.20000000298023224D)
+				.addMotion(0.0D, 0.0035000001080334187D, 0.0D)
+				.randomMotion(0.0010000000474974513D, 0.004999999888241291D)
+				.setMotionCoefficient(0.985F - level.random.nextFloat() * 0.04F)
+				.enableNoClip()
+				.overwriteRemovalProtocol(SpecialRemovalProtocol.ENDING_CURVE_INVISIBLE)
+				.spawn(level, x, y, z);
+				//@formatter:on
+			}
+
+			if(level.random.nextFloat() < 0.25F)
+			{
+				ParticleBuilders.create(ParticleRegistry.SPIRIT_FLAME_PARTICLE)
+				.setScale(0.3F, 0.5F, 0.0F)
+				.setColor(firstColor, secondColor)
+				.setColorEasing(Easing.CIRC_IN_OUT)
+				.setColorCoefficient(3.5F)
+				.setAlpha(0.2F, 1.0F, 0.0F)
+				.setAlphaEasing(Easing.SINE_IN, Easing.CIRC_IN_OUT)
+				.setAlphaCoefficient(3.5F)
+				.randomOffset(0.10000000149011612D, 0.22499999403953552D)
+				.addMotion(0.0D, velocity / 2.0F, 0.0F)
+				.randomMotion(0.0D, 0.014999999664723873D)
+				.setMotionCoefficient(0.97F - level.random.nextFloat() * 0.025F)
+				.enableNoClip()
+				.overwriteRemovalProtocol(SpecialRemovalProtocol.ENDING_CURVE_INVISIBLE)
+				.spawn(level, x, y, z);
+				//@formatter:on
+			}
 		}
 	}
 
