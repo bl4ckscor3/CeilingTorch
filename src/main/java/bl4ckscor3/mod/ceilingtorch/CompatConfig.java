c9bf2486dbea39241f88cc8b8d23b28c6b47b8ea
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-08-25 13:38:29 +0200

    fix crash when at least one supported mod is not present

--- src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
@@ -7,6 +7,7 @@ import com.electronwill.nightconfig.core.file.CommentedFileConfig;
 import com.electronwill.nightconfig.core.io.WritingMode;
 
 import bl4ckscor3.mod.ceilingtorch.compat.additionallights.AdditionalLightsCompat;
+import bl4ckscor3.mod.ceilingtorch.compat.adorn.AdornCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.bonetorch.BoneTorchCompat;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
@@ -47,17 +48,17 @@ public class CompatConfig {
 				"If you turn off a mod integration using this config, you can continue playing without needing an update of Ceiling Torch; However do note that the ceiling torches from that integration will disappear from your world. Note, that if you do not place a block in a space where a ceiling torch was, and then re-enable the respective integration, the torch will reappear.",
 				"Turning off integration with a mod that you are not using will not have any effect, as Ceiling Torch automatically checks for the presence of mods it integrates with.");
 		builtInCompatList = Map.ofEntries(
-				makeEntry(builder, "additional_lights", AdditionalLightsCompat::new),
-				makeEntry(builder, "adorn", AdornCompat::new),
-				makeEntry(builder, "bonetorch", BoneTorchCompat::new));
+				makeEntry(builder, "additional_lights", () -> AdditionalLightsCompat::new),
+				makeEntry(builder, "adorn", () -> AdornCompat::new),
+				makeEntry(builder, "bonetorch", () -> BoneTorchCompat::new));
 		//@formatter:on
 	}
 
-	private Map.Entry<BooleanValue, CompatInfo> makeEntry(ForgeConfigSpec.Builder builder, String modid, Supplier<ICeilingTorchCompat> ceilingTorchCompat) {
+	private Map.Entry<BooleanValue, CompatInfo> makeEntry(ForgeConfigSpec.Builder builder, String modid, Supplier<Supplier<ICeilingTorchCompat>> ceilingTorchCompat) {
 		return Map.entry(builder.define(modid, true), new CompatInfo(modid, ceilingTorchCompat));
 	}
 
-	private Map.Entry<BooleanValue, CompatInfo> makeEntry(ForgeConfigSpec.Builder builder, String modid, String comment, Supplier<ICeilingTorchCompat> ceilingTorchCompat) {
+	private Map.Entry<BooleanValue, CompatInfo> makeEntry(ForgeConfigSpec.Builder builder, String modid, String comment, Supplier<Supplier<ICeilingTorchCompat>> ceilingTorchCompat) {
 		return Map.entry(builder.comment(comment).define(modid, true), new CompatInfo(modid, ceilingTorchCompat));
 	}
 
@@ -69,5 +70,5 @@ public class CompatConfig {
 		return config;
 	}
 
-	public record CompatInfo(String modid, Supplier<ICeilingTorchCompat> ceilingTorchCompat) {}
+	public record CompatInfo(String modid, Supplier<Supplier<ICeilingTorchCompat>> ceilingTorchCompat) {}
 }
