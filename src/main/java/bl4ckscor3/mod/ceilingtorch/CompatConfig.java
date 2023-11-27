bf9875ff5a248b5a56ed8b3a58da514e2f86184b
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-11-27 18:05:23 +0100

    readd projecte support

--- src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
@@ -21,6 +21,7 @@ import bl4ckscor3.mod.ceilingtorch.compat.moshiz.MoShizCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.nethersdelight.NethersDelightCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.occultism.OccultismCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.pokecubeaio.PokecubeAIOCompat;
+import bl4ckscor3.mod.ceilingtorch.compat.projecte.ProjectECompat;
 import bl4ckscor3.mod.ceilingtorch.compat.reliquary.ReliquaryCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.silentgear.SilentGearCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.tofucraft.TofuCraftCompat;
@@ -80,6 +81,7 @@ public class CompatConfig {
 				makeEntry(builder, "ms", "Mo' Shiz Mod", () -> MoShizCompat::new),
 				makeEntry(builder, "nethersdelight", () -> NethersDelightCompat::new),
 				makeEntry(builder, "pokecube_legends", () -> PokecubeAIOCompat::new),
+				makeEntry(builder, "projecte", () -> ProjectECompat::new),
 				makeEntry(builder, "occultism", () -> OccultismCompat::new),
 				makeEntry(builder, "reliquary", () -> ReliquaryCompat::new),
 				makeEntry(builder, "silentgear", () -> SilentGearCompat::new),
---
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/compat/projecte/InterdictionCeilingTorchBlockEntity.java
@@ -0,0 +1,17 @@
+package bl4ckscor3.mod.ceilingtorch.compat.projecte;
+
+import moze_intel.projecte.gameObjs.block_entities.InterdictionTorchBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class InterdictionCeilingTorchBlockEntity extends InterdictionTorchBlockEntity {
+	public InterdictionCeilingTorchBlockEntity(BlockPos pos, BlockState state) {
+		super(pos, state);
+	}
+
+	@Override
+	public BlockEntityType<?> getType() {
+		return ProjectECompat.INTERDICTION_CEILING_TORCH_BLOCK_ENTITY.get();
+	}
+}
