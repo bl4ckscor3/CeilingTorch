c372b4c8205e13b8fdf6b9c6571628e3974491f8
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-05-09 11:49:16 +0200

    add soul bonetorch support

--- src/main/java/bl4ckscor3/mod/ceilingtorch/compat/bonetorch/BoneTorchCompat.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/compat/bonetorch/BoneTorchCompat.java
@@ -3,7 +3,6 @@ package bl4ckscor3.mod.ceilingtorch.compat.bonetorch;
 import java.util.Map;
 
 import com.builtbroken.bonetorch.BoneTorchMod;
-import com.google.common.collect.ImmutableMap;
 
 import bl4ckscor3.mod.ceilingtorch.CeilingTorch;
 import bl4ckscor3.mod.ceilingtorch.ICeilingTorchCompat;
@@ -11,25 +10,18 @@ import bl4ckscor3.mod.ceilingtorch.compat.vanilla.CeilingTorchBlock;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.registries.RegistryObject;
 
 public class BoneTorchCompat implements ICeilingTorchCompat {
-	//@formatter:off
-	public static final RegistryObject<CeilingTorchBlock> CEILING_BONE_TORCH = CeilingTorch.BLOCKS.register("bonetorch_bonetorch", () -> new CeilingTorchBlock(Block.Properties.of(Material.DECORATION)
-			.noCollission()
-			.strength(0.0F)
-			.lightLevel(state -> 14)
-			.sound(SoundType.WOOD),
-			ParticleTypes.FLAME, BoneTorchMod.BONETORCH));
-	//@formatter:on
+	public static final RegistryObject<CeilingTorchBlock> CEILING_BONE_TORCH = CeilingTorch.BLOCKS.register("bonetorch_bonetorch", () -> new CeilingTorchBlock(Block.Properties.copy(Blocks.TORCH), ParticleTypes.FLAME, BoneTorchMod.BONETORCH));
+	public static final RegistryObject<CeilingTorchBlock> CEILING_SOUL_BONE_TORCH = CeilingTorch.BLOCKS.register("bonetorch_soul_bonetorch", () -> new CeilingTorchBlock(Block.Properties.copy(Blocks.SOUL_TORCH), ParticleTypes.SOUL_FIRE_FLAME, BoneTorchMod.SOUL_BONETORCH));
 	private Map<ResourceLocation, Block> placeEntries;
 
 	@Override
 	public Map<ResourceLocation, Block> getPlaceEntries() {
 		if (placeEntries == null)
-			placeEntries = ImmutableMap.of(getRegistryName(BoneTorchMod.BONETORCH.get()), CEILING_BONE_TORCH.get());
+			placeEntries = Map.of(getRegistryName(BoneTorchMod.BONETORCH.get()), CEILING_BONE_TORCH.get(), getRegistryName(BoneTorchMod.SOUL_BONETORCH.get()), CEILING_SOUL_BONE_TORCH.get());
 
 		return placeEntries;
 	}
