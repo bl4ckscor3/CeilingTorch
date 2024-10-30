207a54809d1169518f1e21d1c5d647710faf8e4d
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-10-30 14:46:43 +0100

    Readd Gaia Dimension support

--- src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
@@ -10,6 +10,7 @@ import bl4ckscor3.mod.ceilingtorch.compat.adorn.AdornCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.bambooeverything.BambooEverythingCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.bonetorch.BoneTorchCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.chipped.ChippedCompat;
+import bl4ckscor3.mod.ceilingtorch.compat.gaiadimension.GaiaDimensionCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.occultism.OccultismCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.silentgear.SilentGearCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.tofucraft.TofuCraftCompat;
@@ -44,6 +45,7 @@ public class CompatConfig {
 				makeEntry(builder, "bambooeverything", () -> BambooEverythingCompat::new),
 				makeEntry(builder, "bonetorch", () -> BoneTorchCompat::new),
 				makeEntry(builder, "chipped", () -> ChippedCompat::new),
+				makeEntry(builder, "gaiadimension", () -> GaiaDimensionCompat::new),
 				makeEntry(builder, "occultism", () -> OccultismCompat::new),
 				makeEntry(builder, "silentgear", () -> SilentGearCompat::new),
 				makeEntry(builder, "tofucraft", () -> TofuCraftCompat::new),
