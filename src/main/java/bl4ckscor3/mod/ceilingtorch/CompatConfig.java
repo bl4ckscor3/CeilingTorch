814ecc25ab6b7a5c6674ae236b7b0928c609967e
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-04-02 15:40:06 +0200

    Readd Integrated Dynamics support

--- src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import bl4ckscor3.mod.ceilingtorch.compat.additionallights.AdditionalLightsCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.bonetorch.BoneTorchCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.gaiadimension.GaiaDimensionCompat;
+import bl4ckscor3.mod.ceilingtorch.compat.integrateddynamics.IntegratedDynamicsCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.tofucraft.TofuCraftCompat;
 import net.neoforged.neoforge.common.ModConfigSpec;
 import net.neoforged.neoforge.common.ModConfigSpec.BooleanValue;
@@ -36,6 +37,7 @@ public class CompatConfig {
 				makeEntry(builder, "additional_lights", () -> AdditionalLightsCompat::new),
 				makeEntry(builder, "bonetorch", () -> BoneTorchCompat::new),
 				makeEntry(builder, "gaiadimension", () -> GaiaDimensionCompat::new),
+				makeEntry(builder, "integrateddynamics", () -> IntegratedDynamicsCompat::new),
 				makeEntry(builder, "tofucraft", () -> TofuCraftCompat::new));
 		//@formatter:on
 	}
