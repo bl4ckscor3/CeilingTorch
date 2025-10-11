17129d4c0e53b7c5c21fcfbb9aa027b7ef133642
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-10-11 23:39:05 +0200

    Remove Additional Lights integration until the mod is updated

--- src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
@@ -5,7 +5,6 @@ import java.util.function.Supplier;
 
 import org.apache.commons.lang3.tuple.Pair;
 
-import bl4ckscor3.mod.ceilingtorch.compat.additionallights.AdditionalLightsCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.bonetorch.BoneTorchCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.tofucraft.TofuCraftCompat;
 import net.neoforged.neoforge.common.ModConfigSpec;
@@ -32,7 +31,6 @@ public class CompatConfig {
 				"If you turn off a mod integration using this config, you can continue playing without needing an update of Ceiling Torch; However do note that the ceiling torches from that integration will disappear from your world. Note, that if you do not place a block in a space where a ceiling torch was, and then re-enable the respective integration, the torch will reappear.",
 				"Turning off integration with a mod that you are not using will not have any effect, as Ceiling Torch automatically checks for the presence of mods it integrates with.");
 		builtInCompat = Map.ofEntries(
-				makeEntry(builder, "additional_lights", () -> AdditionalLightsCompat::new),
 				makeEntry(builder, "bonetorch", () -> BoneTorchCompat::new),
 				makeEntry(builder, "tofucraft", () -> TofuCraftCompat::new));
 		//@formatter:on
