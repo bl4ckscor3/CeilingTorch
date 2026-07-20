50181eebf1688975a9dcaf459048e3fc03c723a6
bl4ckscor3 <bl4ckscor33@gmail.com>
2026-07-20 18:03:51 +0200

    Readd Adorn integration

--- src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/CompatConfig.java
@@ -6,6 +6,7 @@ import java.util.function.Supplier;
 import org.apache.commons.lang3.tuple.Pair;
 
 import bl4ckscor3.mod.ceilingtorch.compat.additionallights.AdditionalLightsCompat;
+import bl4ckscor3.mod.ceilingtorch.compat.adorn.AdornCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.bonetorch.BoneTorchCompat;
 import bl4ckscor3.mod.ceilingtorch.compat.tofucraft.TofuCraftCompat;
 import net.neoforged.neoforge.common.ModConfigSpec;
@@ -32,6 +33,7 @@ public class CompatConfig {
 			"Turning off integration with a mod that you are not using will not have any effect, as Ceiling Torch automatically checks for the presence of mods it integrates with.");
 		builtInCompat = Map.ofEntries(
 			makeEntry(builder, "additional_lights", () -> AdditionalLightsCompat::new),
+			makeEntry(builder, "adorn", () -> AdornCompat::new),
 			makeEntry(builder, "bonetorch", () -> BoneTorchCompat::new),
 			makeEntry(builder, "tofucraft", () -> TofuCraftCompat::new));
 	}
---
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/compat/adorn/AdornCompat.java
@@ -0,0 +1,31 @@
+package bl4ckscor3.mod.ceilingtorch.compat.adorn;
+
+import java.util.Map;
+
+import bl4ckscor3.mod.ceilingtorch.CeilingTorch;
+import bl4ckscor3.mod.ceilingtorch.ICeilingTorchCompat;
+import bl4ckscor3.mod.ceilingtorch.compat.vanilla.CeilingTorchBlock;
+import juuxel.adorn.block.AdornBlocks;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.neoforge.registries.DeferredBlock;
+
+public class AdornCompat implements ICeilingTorchCompat {
+	public static final DeferredBlock<CeilingTorchBlock> STONE_CEILING_TORCH = CeilingTorch.BLOCKS.registerBlock("adorn_stone_torch", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, AdornBlocks.STONE_TORCH_GROUND),
+		() -> BlockBehaviour.Properties.ofFullCopy(Blocks.TORCH)
+			.sound(SoundType.STONE)
+			.lightLevel(state -> 15));
+	private Map<Identifier, Block> placeEntries;
+
+	@Override
+	public Map<Identifier, Block> getPlaceEntries() {
+		if (placeEntries == null)
+			placeEntries = Map.of(getRegistryName(AdornBlocks.STONE_TORCH_GROUND.get()), STONE_CEILING_TORCH.get());
+
+		return placeEntries;
+	}
+}
