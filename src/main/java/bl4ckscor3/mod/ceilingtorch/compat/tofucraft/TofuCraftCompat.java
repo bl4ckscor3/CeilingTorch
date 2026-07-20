b60e9ea6e0e9dafffbf0c2d1235f89d7370fa96e
bl4ckscor3 <bl4ckscor33@gmail.com>
2026-07-20 19:09:47 +0200

    Fix compatibility with newer TofuCraftReload releases

--- src/main/java/bl4ckscor3/mod/ceilingtorch/compat/tofucraft/TofuCraftCompat.java
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/compat/tofucraft/TofuCraftCompat.java
@@ -14,56 +14,56 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.neoforge.registries.DeferredBlock;
 
 public class TofuCraftCompat implements ICeilingTorchCompat {
-	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_MOMEN = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_momen", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFUTORCH_MOMEN),
+	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_MOMEN = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_momen", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFU_TORCH_MOMEN),
 		() -> BlockBehaviour.Properties.of()
 			.strength(0.0F, 0.5F)
 			.lightLevel(state -> 14)
 			.noCollision()
 			.noOcclusion()
 			.sound(SoundType.SNOW));
-	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_ISHI = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_ishi", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFUTORCH_ISHI),
+	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_ISHI = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_ishi", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFU_TORCH_ISHI),
 		() -> BlockBehaviour.Properties.of()
 			.strength(0.0F, 6.0F)
 			.lightLevel(state -> 14)
 			.noCollision()
 			.noOcclusion()
 			.sound(SoundType.STONE));
-	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_METAL = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_metal", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFUTORCH_METAL),
+	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_METAL = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_metal", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFU_TORCH_METAL),
 		() -> BlockBehaviour.Properties.of()
 			.strength(0.0F, 7.5F)
 			.lightLevel(state -> 14)
 			.noCollision()
 			.noOcclusion()
 			.sound(SoundType.METAL));
-	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_KINU = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_kinu", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFUTORCH_KINU),
+	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_KINU = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_kinu", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFU_TORCH_KINU),
 		() -> BlockBehaviour.Properties.of()
 			.strength(0.0F, 0.5F)
 			.lightLevel(state -> 14)
 			.noCollision()
 			.noOcclusion()
 			.sound(SoundType.SNOW));
-	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_GRILLED = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_grilled", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFUTORCH_GRILLED),
+	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_GRILLED = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_grilled", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFU_TORCH_GRILLED),
 		() -> BlockBehaviour.Properties.of()
 			.strength(0.0F, 0.5F)
 			.lightLevel(state -> 14)
 			.noCollision()
 			.noOcclusion()
 			.sound(SoundType.SNOW));
-	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_ZUNDA = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_zunda", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFUTORCH_ZUNDA),
+	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_ZUNDA = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_zunda", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFU_TORCH_ZUNDA),
 		() -> BlockBehaviour.Properties.of()
 			.strength(0.0F, 0.5F)
 			.lightLevel(state -> 14)
 			.noCollision()
 			.noOcclusion()
 			.sound(SoundType.SNOW));
-	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_HELL = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_hell", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFUTORCH_HELL),
+	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_HELL = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_hell", p -> new CeilingTorchBlock(p, ParticleTypes.FLAME, TofuBlocks.TOFU_TORCH_HELL),
 		() -> BlockBehaviour.Properties.of()
 			.strength(0.0F, 0.5F)
 			.lightLevel(state -> 14)
 			.noCollision()
 			.noOcclusion()
 			.sound(SoundType.SNOW));
-	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_SOUL = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_soul", p -> new CeilingTorchBlock(p, ParticleTypes.SOUL_FIRE_FLAME, TofuBlocks.TOFUTORCH_SOUL),
+	public static final DeferredBlock<CeilingTorchBlock> TOFU_CEILING_TORCH_SOUL = CeilingTorch.BLOCKS.registerBlock("tofucraft_tofutorch_soul", p -> new CeilingTorchBlock(p, ParticleTypes.SOUL_FIRE_FLAME, TofuBlocks.TOFU_TORCH_SOUL),
 		() -> BlockBehaviour.Properties.of()
 			.strength(0.0F, 0.5F)
 			.lightLevel(state -> 14)
@@ -75,14 +75,14 @@ public class TofuCraftCompat implements ICeilingTorchCompat {
 	@Override
 	public Map<Identifier, Block> getPlaceEntries() {
 		if (placeEntries == null) {
-			placeEntries = Map.of(getRegistryName(TofuBlocks.TOFUTORCH_MOMEN.get()), TOFU_CEILING_TORCH_MOMEN.get(),
-				getRegistryName(TofuBlocks.TOFUTORCH_ISHI.get()), TOFU_CEILING_TORCH_ISHI.get(),
-				getRegistryName(TofuBlocks.TOFUTORCH_METAL.get()), TOFU_CEILING_TORCH_METAL.get(),
-				getRegistryName(TofuBlocks.TOFUTORCH_KINU.get()), TOFU_CEILING_TORCH_KINU.get(),
-				getRegistryName(TofuBlocks.TOFUTORCH_GRILLED.get()), TOFU_CEILING_TORCH_GRILLED.get(),
-				getRegistryName(TofuBlocks.TOFUTORCH_ZUNDA.get()), TOFU_CEILING_TORCH_ZUNDA.get(),
-				getRegistryName(TofuBlocks.TOFUTORCH_HELL.get()), TOFU_CEILING_TORCH_HELL.get(),
-				getRegistryName(TofuBlocks.TOFUTORCH_SOUL.get()), TOFU_CEILING_TORCH_SOUL.get());
+			placeEntries = Map.of(getRegistryName(TofuBlocks.TOFU_TORCH_MOMEN.get()), TOFU_CEILING_TORCH_MOMEN.get(),
+				getRegistryName(TofuBlocks.TOFU_TORCH_ISHI.get()), TOFU_CEILING_TORCH_ISHI.get(),
+				getRegistryName(TofuBlocks.TOFU_TORCH_METAL.get()), TOFU_CEILING_TORCH_METAL.get(),
+				getRegistryName(TofuBlocks.TOFU_TORCH_KINU.get()), TOFU_CEILING_TORCH_KINU.get(),
+				getRegistryName(TofuBlocks.TOFU_TORCH_GRILLED.get()), TOFU_CEILING_TORCH_GRILLED.get(),
+				getRegistryName(TofuBlocks.TOFU_TORCH_ZUNDA.get()), TOFU_CEILING_TORCH_ZUNDA.get(),
+				getRegistryName(TofuBlocks.TOFU_TORCH_HELL.get()), TOFU_CEILING_TORCH_HELL.get(),
+				getRegistryName(TofuBlocks.TOFU_TORCH_SOUL.get()), TOFU_CEILING_TORCH_SOUL.get());
 		}
 
 		return placeEntries;
