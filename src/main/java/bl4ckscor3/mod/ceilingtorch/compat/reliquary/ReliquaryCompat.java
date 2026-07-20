a0420a702d4a01f9fa8c5fca92a1bf03b4d06d83
bl4ckscor3 <bl4ckscor33@gmail.com>
2026-07-20 18:48:48 +0200

    Readd Reliquary Reincarnations integration

---
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/compat/reliquary/ReliquaryCompat.java
@@ -0,0 +1,25 @@
+package bl4ckscor3.mod.ceilingtorch.compat.reliquary;
+
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+
+import bl4ckscor3.mod.ceilingtorch.CeilingTorch;
+import bl4ckscor3.mod.ceilingtorch.ICeilingTorchCompat;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import reliquary.init.ModBlocks;
+
+public class ReliquaryCompat implements ICeilingTorchCompat {
+	public static final DeferredBlock<InterdictionCeilingTorchBlock> INTERDICTION_CEILING_TORCH = CeilingTorch.BLOCKS.registerBlock("reliquary_interdiction_torch", InterdictionCeilingTorchBlock::new);
+	private Map<Identifier, Block> placeEntries;
+
+	@Override
+	public Map<Identifier, Block> getPlaceEntries() {
+		if (placeEntries == null)
+			placeEntries = ImmutableMap.of(getRegistryName(ModBlocks.INTERDICTION_TORCH.get()), INTERDICTION_CEILING_TORCH.get());
+
+		return placeEntries;
+	}
+}
