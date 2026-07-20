433f405fe6ab1e73c9f03cfff8db78084573ba7b
bl4ckscor3 <bl4ckscor33@gmail.com>
2026-07-20 17:10:58 +0200

    Readd Integrated Dynamics integration

---
+++ src/main/java/bl4ckscor3/mod/ceilingtorch/compat/integrateddynamics/MenrilCeilingTorchBlock.java
@@ -0,0 +1,19 @@
+package bl4ckscor3.mod.ceilingtorch.compat.integrateddynamics;
+
+import java.util.function.Supplier;
+
+import bl4ckscor3.mod.ceilingtorch.compat.vanilla.CeilingTorchBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class MenrilCeilingTorchBlock extends CeilingTorchBlock {
+	public MenrilCeilingTorchBlock(Properties properties, Supplier<Block> originalBlock) {
+		super(properties, null, originalBlock);
+	}
+
+	@Override
+	public void animateTick(BlockState state, Level level, BlockPos pos, RandomSource rand) {}
+}
